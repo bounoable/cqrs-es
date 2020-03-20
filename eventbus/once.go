5455edd42750e3663fdb6864c1529421837e401b
Saman Hosseini <bounoable@gmail.com>
2020-03-20 19:12:39 +0100

    add timeout option to eventbus.Once

--- eventbus/once.go
+++ eventbus/once.go
@@ -2,15 +2,38 @@ package eventbus
 
 import (
 	"context"
+	"time"
 
 	"github.com/bounoable/cqrs-es"
 )
 
-// Once ...
-func Once(ctx context.Context, bus cqrs.EventBus, typ cqrs.EventType) (<-chan cqrs.Event, error) {
-	ch := make(chan cqrs.Event, 1)
+// OnceOption ...
+type OnceOption func(*onceConfig)
 
-	ctx, cancel := context.WithCancel(ctx)
+type onceConfig struct {
+	timeout time.Duration
+}
+
+// OnceTimeout ...
+func OnceTimeout(d time.Duration) OnceOption {
+	return func(cfg *onceConfig) {
+		cfg.timeout = d
+	}
+}
+
+// Once ...
+func Once(ctx context.Context, bus cqrs.EventBus, typ cqrs.EventType, opts ...OnceOption) (<-chan cqrs.Event, error) {
+	var cfg onceConfig
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	var cancel context.CancelFunc
+	if cfg.timeout != 0 {
+		ctx, cancel = context.WithTimeout(ctx, cfg.timeout)
+	} else {
+		ctx, cancel = context.WithCancel(ctx)
+	}
 
 	events, err := bus.Subscribe(ctx, typ)
 	if err != nil {
@@ -18,6 +41,7 @@ func Once(ctx context.Context, bus cqrs.EventBus, typ cqrs.EventType) (<-chan cq
 		return nil, err
 	}
 
+	ch := make(chan cqrs.Event, 1)
 	go func() {
 		defer close(ch)
 		defer cancel()
