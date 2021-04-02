21f47b38614adc1dea5096b1a8de9088c5a3c1c1
Saman Hosseini <bounoable@gmail.com>
2021-04-02 15:14:33 +0200

    feat: add `migrate` package

--- go.mod
+++ go.mod
@@ -3,17 +3,14 @@ module github.com/bounoable/cqrs-es
 go 1.14
 
 require (
-	github.com/golang/mock v1.4.3
-	github.com/golang/protobuf v1.4.0 // indirect
-	github.com/google/uuid v1.1.1
+	github.com/golang/mock v1.5.0
+	github.com/google/uuid v1.1.4
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/nats-io/nats-streaming-server v0.17.0 // indirect
-	github.com/nats-io/nats.go v1.9.2
-	github.com/nats-io/stan.go v0.6.0
-	github.com/stretchr/testify v1.5.1
+	github.com/modernice/goes v0.0.0-20210401145204-1d8466381d9c
+	github.com/nats-io/nats.go v1.10.0
+	github.com/nats-io/stan.go v0.8.1
+	github.com/stretchr/testify v1.6.1
 	github.com/tidwall/pretty v1.0.1 // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
-	go.mongodb.org/mongo-driver v1.3.2
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	go.mongodb.org/mongo-driver v1.4.4
 )
---
+++ pkg/migrate/migrate.go
@@ -0,0 +1,76 @@
+package migrate
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/bounoable/cqrs-es"
+	cqrsevent "github.com/bounoable/cqrs-es/event"
+	"github.com/modernice/goes/event"
+)
+
+type Migrator struct {
+	oldstore cqrs.EventStore
+	store    event.Store
+}
+
+func NewMigrator(
+	oldstore cqrs.EventStore,
+	store event.Store,
+) *Migrator {
+	return &Migrator{
+		oldstore: oldstore,
+		store:    store,
+	}
+}
+
+func (m *Migrator) Migrate(ctx context.Context) error {
+	cur, err := m.oldstore.Query(ctx, cqrsevent.Query())
+	if err != nil {
+		return fmt.Errorf("query cqrs Events: %w", err)
+	}
+	defer cur.Close(ctx)
+
+	buf := make(chan cqrs.Event, 100)
+
+	var iter int
+	for cur.Next(ctx) {
+		select {
+		case buf <- cur.Event():
+		default:
+			close(buf)
+			if err := m.insertBuffer(ctx, buf, iter); err != nil {
+				return fmt.Errorf("insert buffer: %w", err)
+			}
+			buf = make(chan cqrs.Event, 100)
+			buf <- cur.Event()
+			iter++
+		}
+	}
+	close(buf)
+
+	if err := m.insertBuffer(ctx, buf, iter); err != nil {
+		return fmt.Errorf("insert buffer: %w", err)
+	}
+
+	if err := cur.Err(); err != nil {
+		return fmt.Errorf("cursor: %w", err)
+	}
+
+	return nil
+}
+
+func (m *Migrator) insertBuffer(ctx context.Context, buf <-chan cqrs.Event, iter int) error {
+	log.Printf("Inserting Events %d-%d...\n", 100*(iter-1), (100*(iter-1))+(len(buf)))
+	var events []event.Event
+	for evt := range buf {
+		events = append(events, event.New(
+			evt.Type().String(),
+			evt.Data(),
+			event.Time(evt.Time()),
+			event.Aggregate(string(evt.AggregateType()), evt.AggregateID(), evt.Version()),
+		))
+	}
+	return m.store.Insert(ctx, events...)
+}
